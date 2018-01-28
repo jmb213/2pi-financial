6f2d2105b4879fba515f2d3624020fc259a21cd8
pwmiller <miller1417@gmail.com>
2018-01-28 15:44:31 -0500

    Change Procfile

--- runserver.py
+++ runserver.py
@@ -1,9 +1,7 @@
 from os import environ
 from app import app
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=80)
-    
+if __name__ == '__main__':    
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '80'))
